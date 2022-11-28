504ce13244a59648ff431348ea566271c6b6e81e
Jac Wynn <jacquinwynn91@gmail.com>
2022-11-27 19:18:41 -0500

    rotate arrow when navigation collapses

--- features/ui/sidebar-navigation/sidebar-navigation.tsx
+++ features/ui/sidebar-navigation/sidebar-navigation.tsx
@@ -147,6 +147,9 @@ const LinkList = styled(List)`
 const CollapseMenuItem = styled(MenuItemButton)`
   display: none;
 
+  transform: ${({ isCollapsed }) =>
+    isCollapsed ? "rotate(180deg)" : "rotate(0)"};
+
   @media (min-width: ${breakpoint("desktop")}) {
     display: flex;
   }
